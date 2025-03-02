1b1023b0e769b5fb3cd5f35094ca086cca13bb89
Keval Shah <keval.arhan@gmail.com>
2025-03-02 02:21:08 -0700

    remove unused files and configurations

--- lib/db/migrations/0006_odd_loners.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "DocumentChunk" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"documentId" uuid NOT NULL,
-	"content" text NOT NULL,
-	"chunk_vector" vector(1536) NOT NULL
-);
